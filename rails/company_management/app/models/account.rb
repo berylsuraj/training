40f07c1487ae6c08422256bf54ae6b96acf4872d
Suraj <appdev@webulseindia.com>
2022-02-12 18:31:17 +0530

    Added custom validators

--- rails/company_management/app/models/account.rb
+++ rails/company_management/app/models/account.rb
@@ -1,5 +1,7 @@
 class Account < ApplicationRecord
 	belongs_to :employee
 	has_one :account_history
-  
+  validates :employee_id, presence: true, uniqueness: true
+  include ActiveModel::Validations
+  validates_with MyValidator
 end
---
+++ rails/company_management/app/models/validators/my_validator.rb
@@ -0,0 +1,8 @@
+class MyValidator < ActiveModel::Validator
+
+  def validate(record)
+    unless.record.account_num.length>=7 && record.account_num.scan(/\D/)
+      record.errors.add :name, "Enter a valid acount number "
+    end
+  end
+end
