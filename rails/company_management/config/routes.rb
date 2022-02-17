589d6e0375616ad26537b07b66589b1f8e35c1be
Suraj <appdev@webulseindia.com>
2022-02-17 19:42:37 +0530

    Added all employee page

--- rails/company_management/config/routes.rb
+++ rails/company_management/config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
    root "home#index"
    get '/employee/new', to:'employees#new'
    get '/employee/create', to:'employees#create'
-   get '/employee/:employee_id', to: 'employees#show'
+   get '/employee/all', to:'employees#index'
+   #get '/employee/:employee_id', to: 'employees#show'
    get '/department/new', to: 'department#new'
    get '/department/create', to: 'department#create'
 end
